02b310b12544496c7e2d5c35a5e96bfcd276194b
Dustin Haefele <Dustin.haefele@gmail.com>
2019-08-14 15:53:57 -0400

    connecting to api and getting industries and donation summary information

--- src/config.js
+++ src/config.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL
+  OPEN_SECRETS_BASE_URL: 'http://www.opensecrets.org/api/?',
+  DB_URL: process.env.DB_URL
     || 'postgresql://dunder-mifflin@localhost/stay-informed',
+  OPEN_SECRETS_API_KEY: process.env.OPEN_SECRETS_API_KEY || '44dfdfb8e6fad2e771150bcc022a5a98',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '180h', /* TODO revisit expiry time */
-}
+};
---
+++ src/politicians/politicians-service.js
@@ -0,0 +1,43 @@
+const config = require('../config');
+const fetch = require('node-fetch');
+
+const politiciansService = {
+  //This gets the total contributions and spending by cid #
+  getContributionTotals(cid) {
+    const url = `${
+      config.OPEN_SECRETS_BASE_URL
+    }method=candSummary&cid=${cid}&output=json&apikey=${
+      config.OPEN_SECRETS_API_KEY
+    }`;
+
+    return fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        return {
+          total_donations: res.response.summary['@attributes'].total,
+          spent: res.response.summary['@attributes'].spent,
+          cash_on_hand: res.response.summary['@attributes'].cash_on_hand
+        };
+      });
+  },
+
+  getTopIndustries(cid) {
+    const url = `${config.OPEN_SECRETS_BASE_URL}method=candIndustry&cid=${cid}&output=json&apikey=${config.OPEN_SECRETS_API_KEY}`;
+
+    return fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        let industries = [];
+
+        res.response.industries.industry.forEach(ind => {
+          industries.push(ind['@attributes']);
+
+        });
+        console.log(industries);
+      });
+  },
+};
+
+console.log(politiciansService.getTopIndustries('N00030780'));
+
+module.exports = politiciansService;
