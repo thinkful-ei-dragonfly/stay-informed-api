2960b6eb2dbd429c659eed07c47ef7b5e504407a
Robert Wiggins <wigginsro11@gmail.com>
2019-08-20 17:50:59 -0400

    functional route to news api searching reps

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const { NODE_ENV } = require('./config');
 const errorHandler = require('./middleware/error-handler');
 const authRouter = require('./auth/auth-router');
 const userRouter = require('./user/user-router');
+const newsRouter = require('./news/news-router')
 const representativeRouter = require('./representative/representative-router')
 
 
@@ -22,7 +23,7 @@ app.use(helmet());
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/representatives', representativeRouter);
-app.use('api/news', newsRouter)
+app.use('/api/news', newsRouter)
 
 
 app.use(errorHandler);
--- src/news/news-router.js
+++ src/news/news-router.js
@@ -1,55 +1,85 @@
 const express = require('express');
 const path = require('path');
-const { NEWS_API_KEY } = require('./config');
+const config = require('../config');
 const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI(NEWS_API_KEY);
-
+const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
+// const newsService = require('./news-service')
 const newsRouter = express.Router();
 const jsonBodyParser = express.json();
 
-newsRouter.post('/', jsonBodyParser, (req, res, next) => {
-  const { senator1, senator2, representative1 } = req.body;
+// TODO refactor logic in this function to make it cleaner and more readable
+// TODO still use the module? could perhaps handle ourselves.
+newsRouter
+  .post('/', jsonBodyParser, (req, res, next) => {
+    const { senator1, senator2, representative1 } = req.body;
+    console.log('senator1: ', senator1)
+    // join all politican names in order to make an efficient fetch and collect articles about all
+    // TODO consider only last names? test.
+    // Fetch requires URL encoded, but I think module is handling
+    const queryString = `${senator1} ${senator2} ${representative1}`;
+    console.log('queryString: ', queryString)
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
 
-  // join all politican names in order to make an efficient fetch and collect articles about all
-  // TODO consider only last names? test.
-  // Fetch requires URL encoded, but I think module is handling
-  const queryString = `${senator1} ${senator2} ${representative1}`
-    
-  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    
-  // get today's date in string form YYYY-MM-DD
-    let dateObj = new Date();
-    let today = dateObj;
-    const publishStartDateStr = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
-    
-    // currently using a 7 day trailing period, i.e., get articles within the last week
-    let trailingDateObj = new Date();
-    trailingDateObj.setDate(trailingDateObj.getDate() - 7);
-    const publishTrailingStr = `${trailingDateObj.getFullYear()}-${trailingDateObj.getMonth() + 1}-${trailingDateObj.getDate()}`;
-    console.log('publishStartDateStr: ', publishStartDateStr)
-    console.log('trailingStartDateStr: ', publishTrailingStr)
+    // get today's date in string form YYYY-MM-DD
+    // let dateObj = new Date();
+    // let today = dateObj;
+    // // TODO REMOVE TEMP ZERO
+    // const publishStartDateStr = `${today.getFullYear()}-0${today.getMonth() +
+    //   1}-${today.getDate()}`;
+
+    // // currently using a 7 day trailing period, i.e., get articles within the last week
+    // let trailingDateObj = new Date();
+    // trailingDateObj.setDate(trailingDateObj.getDate() - 7);
+    // // TODO REMOVE TEMP ZERO
+    // const publishTrailingStr = `${trailingDateObj.getFullYear()}-0${trailingDateObj.getMonth() +
+    //   1}-${trailingDateObj.getDate()}`;
+    // console.log('publishStartDateStr: ', publishStartDateStr);
+    // console.log('trailingStartDateStr: ', publishTrailingStr);
 
     //  To query everything
     //  You must include at least one q, source, or domain
     //  TODO consider q advanced search specifications i.e. OR or MUST APPEAR
-     newsapi.v2
-       .everything({
-         q: queryString,
+    newsapi.v2
+      .everything({
+        q: queryString,
         //  sources: 'bbc-news,the-verge',
-         // domains: 'bbc.co.uk,techcrunch.com',
-         from: publishStartDateStr,
-         to: publishTrailingStr,
-         language: 'en',
-         sortBy: 'relevancy',
-         page: 1,  // Huh? Page? 0 based?
-       })
-       .then(response => {
-         console.log(response);
-         // TODO 
-         return response.status(201).json()    
-       })
-       .catch(error => console.error(error.message));
+        // domains: 'bbc.co.uk,techcrunch.com',
+        // from: publishStartDateStr,
+        // to: publishTrailingStr,
+        language: 'en',
+        sortBy: 'relevance',
+        page: 1,
+      })
+      .then(response => {
+        // console.log('RESPONSE: ', response);
+        // let resWithSubject = newsService.findName(response.articles);
+        
+        return res.status(201).send(response);
+      })
+      .catch(error => {
+        console.error(error.message)
+        return res.json({error: error.message})
+      });
   })
-  .catch(error => {
-    res.status(400).send({ message: 'oops! Something went wrong with news fetch.'})
-  });
+
+
+  // .catch(error => {
+  //   res
+  //     .status(400)
+  //     .send({ message: 'oops! Something went wrong with news fetch.' });
+  // });
+
+module.exports = newsRouter;
