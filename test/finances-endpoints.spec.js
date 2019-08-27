640db862c28c15834a14bbcdc230f668120cc6e4
Dustin Haefele <Dustin.haefele@gmail.com>
2019-08-27 11:46:13 -0400

    tests passing

--- test/finances-endpoints.spec.js
+++ test/finances-endpoints.spec.js
@@ -2,23 +2,23 @@ const app = require('../src/app');
 const helpers = require('./test-helpers');
 
 
-describe.only('Finances Endpoints', function() {
+describe('Finances Endpoints', function() {
   const testCid = 'N00038767';
   //Increasing the timeout because the three chained api calls take some time.
   this.timeout(7000);
   describe(`POST /api/finances`, () => { 
-    it(`responds 400 and with correct error when address key provided but no value`, () => {
+    it(`responds 400 and with correct error when cid key provided but no value`, () => {
       return supertest(app)
         .post('/api/finances')
-        .send({cip:''})
+        .send({cid:''})
         .expect(400, {error: 'Must include crp id in request body'});
     });
-    it(`responds 400 and with correct error when no address key provided`, () => {
+    it(`responds 400 and with correct error when no cid key provided`, () => {
       return supertest(app)
         .post('/api/finances')
         .expect(400, {error: 'Must include crp id in request body'});
     });
-    it(`responds 200 and with representative data`, () => {
+    it(`responds 200 and with financial data`, () => {
       return supertest(app)
         .post('/api/finances')
         .send({cid: testCid})
